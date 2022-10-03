9eab093fac8680231cd6a5e130bb15bfa806395e
daikifujioka <taiqiaot@gmail.com>
2022-10-04 01:12:45 +0900

    create task model

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to task_url(@task), notice: "task was successfully created." }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :text, :start_time, :end_time)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_many :tasks
+
+  def self.todays_task(users)
+    @tasks = []
+    users.each do |user|
+      @tasks.append(user.tasks.where(start_time: Time.current.at_beginning_of_day..Time.current.at_end_of_day))
+    end
+
+    @tasks
+  end
 end
