bb0cc6dfbea8ef43d461345f1fee66087316b603
daikifujioka <taiqiaot@gmail.com>
2022-10-04 22:04:51 +0900

    feat: made getting user's tasks among a day, a week, and a month

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,41 @@
 class User < ApplicationRecord
   has_many :tasks
 
-  def self.todays_task(users)
-    @tasks = []
-    users.each do |user|
-      @tasks.append(user.tasks.where(start_time: Time.current.at_beginning_of_day..Time.current.at_end_of_day))
+  validates :start_time, presence: true
+  validates :end_time,   presence: true
+
+  def between_time_tasks(start, finish)
+    range = start..finish
+    self.tasks.where(start_time: range, end_time: range)
+  end
+
+  def today_tasks()
+    between_time_tasks(Time.current.at_beginning_of_day, Time.current.at_end_of_day)
+  end
+
+  def this_week_tasks()
+    sunday = Time.current.at_beginning_of_day
+    unless sunday.sunday? 
+      sunday -= 86400
     end
 
-    @tasks
+    between_time_tasks(sunday, sunday + 86400 * 7)
   end
+
+  def this_month_tasks()
+    today     = Time.current
+    first_day = Time.new(today.year, today.month,   1, 0, 0, 0)
+    last_day  = Time.new(today.year, today.month+1, 1, 0, 0, 0) - 1
+
+    between_time_tasks(first_day, last_day)
+  end
+
+  # def member_todays_task(users)
+  #   @tasks = []
+  #   users.each do |user|
+  #     @tasks.append(user.tasks.today_tasks())
+  #   end
+
+  #   @tasks
+  # end
 end
